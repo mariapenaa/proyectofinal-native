88342e50dd17677ba42d0b5e6cd835ad14562d1b
catalinaarias01 <63122855+catalinaarias01@users.noreply.github.com>
2021-11-09 21:41:32 -0300

    New post

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,26 +1,72 @@
-import React, {Component} from "react";
-import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {Component} from 'react';
+import {Text, TouchableOpacity, View, StyleSheet, Image, ActivityIndicator, FlatList, TextInput} from 'react-native';
+import { db, auth } from '../firebase/config';
+import Post from '../components/Post';
+
 
 class Home extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-        
-        }
+  constructor(props){
+    super(props);
+    this.state ={
+      posteos: [],
     }
+  }
+  componentDidMount(){
+    db.collection('posts').onSnapshot(
+      docs => {
+        console.log(docs);
+        //Array para crear datos en formato más útil.
+        let posts = [];
+        docs.forEach( doc => {
+          posts.push({
+            id: doc.id,   
+            data: doc.data(),
+          })
+        })
+        console.log(posts);
 
+        this.setState({
+          posteos: posts,
+        })
+      }
+    )
+  }
 
-    render(){
-        return(
-            <View>
-
-            </View>
-        )
-    }
+  render(){
+    return(
+      <View style={styles.container}>
+        <FlatList 
+          data= { this.state.posteos }
+          keyExtractor = { post => post.id}
+          renderItem = { ({item}) => <Post postData={item} />} 
+        />
+      </View>
+      )
+  }
 }
 
 const styles = StyleSheet.create({
-
+  container:{
+    paddingHorizontal:10,
+  },
+  formContainer:{
+    backgroundColor: '#ccc',
+    marginHorizontal: 10,
+    padding:10,
+  },
+  field:{
+    borderColor: '#444',
+    borderWidth:1,
+    borderStyle: 'solid',
+    height: 20,
+    paddingHorizontal: 20,
+    paddingVertical:10
+  },
+  touchable:{
+    backgroundColor: '#ccc',
+    borderRadius:4,
+    marginVertical:10,
+  }
 })
 
 export default Home;
